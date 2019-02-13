3329c93693f38a640a5f3e37eaea80eb5ac0ea39
Bridget Sarah <msbskelly@gmail.com>
2019-02-13 10:22:58 +0000

    Programming in Python 3 - P13

---
+++ Examples/structure.py
@@ -0,0 +1,31 @@
+#Pythons Structure
+
+
+#object reference  (objectreference = value)
+x = "blue" ##creates a string "object" with the text "blue"
+y = "green"
+z = x
+
+
+# = # (the '=' operator binds an object reference to an object in memory)
+
+
+
+route = 866
+print(route, type(route)) #prints: 866 <class 'int'>
+route = "north"
+print(route, type(route)) #prints:866 <class 'str'>
+
+#type() - function returns the data type, also know as the class of the data item
+#---! Only can be used in Python Shell, useful for Testing/Debugging, alternativly use [print()] outside shell.
+
+
+##tuple example!! 
+#---! [doesn't store data types] --!#
+##('blue, 'green', 'blue')
+
+####Creating Lists#### 
+#---! [doesn't store data types] --!#
+####[1, 2, 3, 4, 5]
+####['alpha', 'bravo', 'charlie'
+####['zebra, 49, -987, 'elephant',200]
