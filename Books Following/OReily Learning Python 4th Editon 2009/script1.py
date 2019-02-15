5db427e74d4509131c4536ce1e2e4b0cea889c79
Bridget Sarah <msbskelly@gmail.com>
2019-02-15 11:08:44 +0000

    Introduction to Python Scripting

---
+++ Books Following/OReily Learning Python 4th Editon 2009/script1.py
@@ -0,0 +1,6 @@
+# a first python script
+import sys              #load a library module
+print(sys.platform)     #sys module
+print(2 ** 100)         #raise 2 to a power
+x = 'spam!'
+print(x * 8)            #string repetition
